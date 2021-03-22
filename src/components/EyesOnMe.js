9c13615f2d85db5e7c8091d7da42d2a8c163d468
Nicholas V Frangione <nfrangione8@gmail.com>
2021-03-22 09:43:41 -0700

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default class Keyboard extends React.Component{
+    onFocus = () => console.log('Good!')
+
+    onBlur = () => console.log('Hey! Eyes on me!')
+
+    render() {
+        return (
+            <div>
+                <button onFocus = {this.onClick} onBlur={this.onBlur}/>
+            </div>
+        )
+    }
+}
